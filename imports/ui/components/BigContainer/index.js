0581e56ee918d2c8823a64e8eda25104747f8d67
Warren Chan <warrenchan@warren-2.local>
2017-05-12 15:29:42 -0700

    BigContainer + added uni sans font

---
+++ imports/ui/components/BigContainer/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './styles.css'
+
+export default class BigContainer extends React.Component {
+
+
+
+  render() {
+    return (
+      <div className="bigcontainer">
+          <div className="containerbanner">
+              <h1>title</h1>
+          </div>
+      </div>
+    );
+  }
+}
