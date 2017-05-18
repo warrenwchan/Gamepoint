6a4b27e3e0d748598de1629ce04a65c5e449273b
Warren Chan <warrenchan@warren-2.local>
2017-05-18 09:25:20 -0700

    changing state to display search input

--- imports/ui/components/GreenButton/index.js
+++ imports/ui/components/GreenButton/index.js
@@ -4,9 +4,7 @@ import styles from './styles.css'
 export default class GreenButton extends Component {
   render() {
     return (
-      <div>
-          <button className="greenButton"> {this.props.title} </button>
-      </div>
+      <button className="greenButton"> {this.props.title} </button>
     );
   }
 }
--- imports/ui/containers/Profile/SearchPlayers/index.js
+++ imports/ui/containers/Profile/SearchPlayers/index.js
@@ -4,7 +4,21 @@ import styles from './styles.css';
 import GreenButton from './../../../components/GreenButton';
 
 class SearchPlayers extends Component {
-    // state = {  }
+    constructor() {
+        super();
+        this.state =  {
+            search: '',
+        };
+    }
+
+    updateSearch(event) {
+        this.setState({search: event.target.value})
+    }
+
+    // addFriend(userName) {
+    //     Meteor.users.update({_id: userId()}, { $push: { 'profile.friends': "userName"}});
+    // }
+
     render() {
         return (
             <div className="freindsSections">
@@ -12,12 +26,26 @@ class SearchPlayers extends Component {
                     <button className="searchExit" onClick={() => this.props.onClick()}>
                         <i className="fa fa-times" aria-hidden="true"></i>
                     </button>
-                    <input type="text"/>
-                    <GreenButton title="Search"/>
+                    <input
+                        className="searchInput"
+                        type="text" placeholder="Search Friends"
+                        value={this.state.search}
+                        onChange={this.updateSearch.bind(this)}
+                    />
+                    <GreenButton
+                        className="searchButton"
+                        title="Search"
+                    />
                 </div>
                 <div className="friendsResult">
                     <h1>Results</h1>
-                    <p>player 1</p>
+                    <ul id="searchResults">
+                        <li>{this.state.search}
+                            <button className="searchResult">
+                                <i class="fa fa-plus" aria-hidden="true"></i>
+                            </button>
+                        </li>
+                    </ul>
                 </div>
             </div>
         );
